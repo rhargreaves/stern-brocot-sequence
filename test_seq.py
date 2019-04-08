3de293c053879a334bfe93a2c4d84dce482bb09f
rhargreaves <robert@roberthargreaves.net>
2019-04-08 22:22:14 +0100

    Start Fibonacci function

--- test_seq.py
+++ test_seq.py
@@ -1,10 +1,17 @@
 from fractions import Fraction
+import pytest
+
+
+def fibonacci(index):
+    if index == 0:
+        return 0
 
 
 def stern_brocot(count):
     return []
 
 
+@pytest.mark.skip(reason="Will fail until implementation started")
 def test_returns_first_5_items():
     seq = stern_brocot(5)
     assert seq == [
@@ -12,5 +19,8 @@ def test_returns_first_5_items():
         Fraction(1, 2),
         Fraction(2, 1),
         Fraction(1, 3),
-        Fraction(3, 2)
-    ]
+        Fraction(3, 2)]
+
+
+def test_fibonacci_returns_zero():
+    assert fibonacci(0) == 0
