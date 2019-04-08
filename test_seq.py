5a4c21ad0bb78659dd1a4dd88f26fe2289789a04
rhargreaves <robert@roberthargreaves.net>
2019-04-08 22:13:33 +0100

    Conform to pep8

--- test_seq.py
+++ test_seq.py
@@ -1,14 +1,16 @@
 from fractions import Fraction
 
+
 def stern_brocot(count):
     return []
 
+
 def test_returns_first_5_items():
-    seq = stern_brocot(5);
+    seq = stern_brocot(5)
     assert seq == [
-            Fraction(1,1),
-            Fraction(1,2),
-            Fraction(2,1),
-            Fraction(1,3),
-            Fraction(3,2)
-            ]
+        Fraction(1, 1),
+        Fraction(1, 2),
+        Fraction(2, 1),
+        Fraction(1, 3),
+        Fraction(3, 2)
+    ]
