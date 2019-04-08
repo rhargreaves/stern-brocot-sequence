3f4fe5bcf7d16f9be2044df1109026c2b9ff455a
rhargreaves <robert@roberthargreaves.net>
2019-04-08 22:54:33 +0100

    Refactor

--- test_seq.py
+++ test_seq.py
@@ -3,10 +3,8 @@ import pytest
 
 
 def fibonacci(index):
-    if index == 0:
-        return 0
-    if index == 1:
-        return 1
+    if index == 0 or index == 1:
+        return index
     return fibonacci(index-1) + fibonacci(index-2)
 
 
