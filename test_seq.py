3df5e2d63e35f48f9687d4534c67d927ac32e862
rhargreaves <robert@roberthargreaves.net>
2019-04-08 22:02:23 +0100

    Basic failing test

---
+++ test_seq.py
@@ -0,0 +1,18 @@
+from fractions import Fraction
+
+half = Fraction(1, 2)
+
+def stern_brocot(count):
+    return []
+
+def test_returns_first_5_items():
+    seq = stern_brocot(5);
+    assert seq == [
+            Fraction(1,1),
+            Fraction(1,2),
+            Fraction(2,1),
+            Fraction(1,3),
+            Fraction(3,2)
+            ]
+
+print(half);
