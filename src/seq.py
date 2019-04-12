af91d3fda0eb26dd337e7b871c35820a7c12d9bb
rhargreaves <robert@roberthargreaves.net>
2019-04-12 21:48:49 +0100

    Ensure 'weird' fib sequence function skips 0

--- src/seq.py
+++ src/seq.py
@@ -1,3 +1,5 @@
+
+
 def fibonacci(index):
     if index == 0 or index == 1:
         return index
@@ -6,6 +8,11 @@ def fibonacci(index):
 
 def fib_seq(n):
     for i in range(0, n+1):
+        yield fibonacci(i)
+
+
+def weird_fib_seq(n):
+    for i in range(1, n+2):
         yield fibonacci(i)
 
 
---
+++ test_seq.py
@@ -0,0 +1,42 @@
+from fractions import Fraction
+from src.seq import fibonacci, weird_fib_seq, fib_seq, stern_brocot
+import pytest
+
+
+@pytest.mark.skip(reason="Will fail until implementation started")
+def test_returns_first_5_items():
+    seq = stern_brocot(5)
+    assert seq == [
+        Fraction(1, 1),
+        Fraction(1, 2),
+        Fraction(2, 1),
+        Fraction(1, 3),
+        Fraction(3, 2)]
+
+
+def test_fibonacci_weird_seq_skips_zero_for_n():
+    assert list(weird_fib_seq(2)) == [1, 1, 2]
+
+
+def test_fibonacci_seq_correct_for_zero():
+    assert list(fib_seq(0)) == [0]
+
+
+def test_fibonacci_seq_correct_for_one():
+    assert list(fib_seq(1)) == [0, 1]
+
+
+def test_fibonacci_seq_correct_for_n():
+    assert list(fib_seq(7)) == [0, 1, 1, 2, 3, 5, 8, 13]
+
+
+def test_fibonacci_correct_for_zero():
+    assert fibonacci(0) == 0
+
+
+def test_fibonacci_correct_for_one():
+    assert fibonacci(1) == 1
+
+
+def test_fibonacci_correct_for_n():
+    assert fibonacci(7) == 13
