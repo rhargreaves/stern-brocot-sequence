fb1e7bdefc56357e1e150420a5ec6074f76ba48d
rhargreaves <robert@roberthargreaves.net>
2019-04-13 13:13:01 +0100

    Correct take function usage

--- tests/test_seq.py
+++ tests/test_seq.py
@@ -5,11 +5,11 @@ import pytest
 
 
 def take(func, n):
-    return list(islice(func(), n+1))
+    return list(islice(func(), n))
 
 
 def test_returns_first_5_items():
-    seq = take(stern_brocot, 4)
+    seq = take(stern_brocot, 5)
     assert seq == [
         Fraction(1, 1),
         Fraction(1, 2),
@@ -19,28 +19,28 @@ def test_returns_first_5_items():
 
 
 def test_fibonacci_weird_seq_appends_previous_fib_result():
-    assert take(weird_fib_seq, 4) == [0, 1, 1, 2, 1]
+    assert take(weird_fib_seq, 5) == [0, 1, 1, 2, 1]
 
 
 def test_fibonacci_weird_seq_appends_previous_fib_result_for_higher_n():
-    assert take(weird_fib_seq, 9) == [0, 1, 1, 2, 1, 3, 2, 3, 1, 4]
+    assert take(weird_fib_seq, 10) == [0, 1, 1, 2, 1, 3, 2, 3, 1, 4]
 
 
 def test_fibonacci_weird_seq_appends_previous_fib_result_for_much_higher_n():
-    assert take(weird_fib_seq, 15) == [
+    assert take(weird_fib_seq, 16) == [
         0, 1, 1, 2, 1, 3, 2, 3, 1, 4, 3, 5, 2, 5, 3, 4]
 
 
 def test_fibonacci_seq_correct_for_zero():
-    assert take(fib_seq, 0) == [0]
+    assert take(fib_seq, 1) == [0]
 
 
 def test_fibonacci_seq_correct_for_one():
-    assert take(fib_seq, 1) == [0, 1]
+    assert take(fib_seq, 2) == [0, 1]
 
 
 def test_fibonacci_seq_correct_for_n():
-    assert take(fib_seq, 7) == [0, 1, 1, 2, 3, 5, 8, 13]
+    assert take(fib_seq, 8) == [0, 1, 1, 2, 3, 5, 8, 13]
 
 
 def test_fibonacci_correct_for_zero():
